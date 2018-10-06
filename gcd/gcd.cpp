79618af5dde2fbf50ec191cca60623fda09ef35b
Hewla ひゅーら <43031662+hewlae@users.noreply.github.com>
2018-10-06 22:01:58 +0900

    Update gcd.cpp

--- gcd/gcd.cpp
+++ gcd/gcd.cpp
@@ -1,9 +1,10 @@
+//gcd
 long gcd(long num1, long num2){
 	if(num2 > num1){
 		std::swap(num1, num2);
 	}
-	if(num1 % num2 == 0){
-		return num2;
+	if(num2 == 0){
+		return num1;
 	}else{
 		return gcd(num2, num1 % num2);
 	}
